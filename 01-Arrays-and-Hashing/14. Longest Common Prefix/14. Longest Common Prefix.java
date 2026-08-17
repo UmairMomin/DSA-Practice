46deb276845694e26446e2c12e8e835178b2ce8d
Umair <66427095+UmairMomin@users.noreply.github.com>
2026-08-18 01:02:21 +0530

    [14] [Time Beats: 100.00%] [Memory Beats: 83.32%] - Leet2Hub

--- 01-Arrays-and-Hashing/14. Longest Common Prefix/14. Longest Common Prefix.java
+++ 01-Arrays-and-Hashing/14. Longest Common Prefix/14. Longest Common Prefix.java
@@ -1,1 +1,18 @@
-class Solution {    public String longestCommonPrefix(String[] strs) {                if(strs.length == 0 ) return ;        String prefix = strs[0];        for(int i = 0; i < strs.length; i++){            while(strs[i].indexOf(prefix) != 0 ){                prefix = prefix.substring(0, prefix.length() - 1);                if(prefix.isEmpty()) return ;            }        }        return prefix;    }}
+1class Solution {
+2    public String longestCommonPrefix(String[] strs) {
+3        
+4        if(strs.length == 0 ) return ;
+5
+6        String prefix = strs[0];
+7
+8        for(int i = 0; i < strs.length; i++){
+9            while(strs[i].indexOf(prefix) != 0 ){
+10                prefix = prefix.substring(0, prefix.length() - 1);
+11                if(prefix.isEmpty()) return ;
+12            }
+13        }
+14
+15        return prefix;
+16
+17    }
+18}
